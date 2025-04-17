28265bc28fad6297f9962359655d2f758ae32052
Juliet WANG <informathemusic@gmail.com>
2025-04-17 16:32:06 +0200

    Not using as mitm anymore

--- src/compositor.ts
+++ src/compositor.ts
@@ -57,45 +57,45 @@ export class Compositor extends EventEmitter<CompositorEvents> {
         console.log("New Connection!!!");
 
         // - As compositor
-        // this.emit('connection', new Connection(this.currConnId++, this, socket, false));
+        this.emit('connection', new Connection(this.currConnId++, this, socket, false));
 
         // - As MITM
-        const socket2 = new USocket({});
-        const conx = new Connection(this.currConnId++, this, socket, true);
-        socket2.connect({ path: "/run/user/1000/wayland-0" }, () => {
-          socket.on("readable", () => {
-            parseOnReadable(socket, ({ data, fds }) => {
-              // console.log("C2S", data && data.toString("hex"), fds);
-              try {
-                const [[a, b, c]] = [...conx.parser(data)];
-                if (!blacklist.includes(a.iface)) {
-                  console.log("C2S", Connection.prettyWlObj(a), b, JSON.stringify(Connection.prettyArgs(c)));
-                }
-                socket2.write({ data, fds });
-              } catch (e) {
-                console.log('C2S got error', e);
-                socket2.write({ data, fds });
-              }
-            });
-          });
-          socket2.on("readable", () => {
-            parseOnReadable(socket2, ({ data, fds }) => {
-              // console.log("S2C", data && data.toString("hex"), fds);
-              try {
-                const [[a, b, c]] = [...conx.parser(data, true)];
-                if (!blacklist.includes(a.iface)) {
-                  console.log("S2C", Connection.prettyWlObj(a), b, JSON.stringify(Connection.prettyArgs(c)));
-                }
-                if (b === 'global' && !WlRegistry.supportedByRegistry.includes(c.interface)) return console.log('Ignoring');
-                // if (!WlRegistry.registry.includes(c.))
-                socket.write({ data, fds });
-              } catch (e) {
-                console.log('S2C got error', e);
-                socket.write({ data, fds });
-              }
-            });
-          });
-        });
+        // const socket2 = new USocket({});
+        // const conx = new Connection(this.currConnId++, this, socket, true);
+        // socket2.connect({ path: "/run/user/1000/wayland-0" }, () => {
+        //   socket.on("readable", () => {
+        //     parseOnReadable(socket, ({ data, fds }) => {
+        //       // console.log("C2S", data && data.toString("hex"), fds);
+        //       try {
+        //         const [[a, b, c]] = [...conx.parser(data)];
+        //         if (!blacklist.includes(a.iface)) {
+        //           console.log("C2S", Connection.prettyWlObj(a), b, JSON.stringify(Connection.prettyArgs(c)));
+        //         }
+        //         socket2.write({ data, fds });
+        //       } catch (e) {
+        //         console.log('C2S got error', e);
+        //         socket2.write({ data, fds });
+        //       }
+        //     });
+        //   });
+        //   socket2.on("readable", () => {
+        //     parseOnReadable(socket2, ({ data, fds }) => {
+        //       // console.log("S2C", data && data.toString("hex"), fds);
+        //       try {
+        //         const [[a, b, c]] = [...conx.parser(data, true)];
+        //         if (!blacklist.includes(a.iface)) {
+        //           console.log("S2C", Connection.prettyWlObj(a), b, JSON.stringify(Connection.prettyArgs(c)));
+        //         }
+        //         if (b === 'global' && !WlRegistry.supportedByRegistry.includes(c.interface)) return console.log('Ignoring');
+        //         // if (!WlRegistry.registry.includes(c.))
+        //         socket.write({ data, fds });
+        //       } catch (e) {
+        //         console.log('S2C got error', e);
+        //         socket.write({ data, fds });
+        //       }
+        //     });
+        //   });
+        // });
       }.bind(this),
     );
 
