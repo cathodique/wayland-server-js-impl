1fab339b793714d8fc2a0c2c9f7210793f9e73a2
Juliet WANG <informathemusic@gmail.com>
2025-04-17 16:32:43 +0200

    build

--- dist/compositor.js
+++ dist/compositor.js
@@ -11,9 +11,6 @@ const logger_js_1 = require("./logger.js");
 const connection_js_1 = require("./connection.js");
 const usocket_1 = require("@cathodique/usocket");
 const node_events_1 = __importDefault(require("node:events"));
-// import { ExistentParent } from "./objects/base_object.js";
-// import { newIdMap } from "./new_id_map.js";
-const wl_registry_js_1 = require("./objects/wl_registry.js");
 const blacklist = ['wl_keyboard'];
 class Compositor extends node_events_1.default {
     server;
@@ -31,47 +28,44 @@ class Compositor extends node_events_1.default {
             // Comment one:
             logger_js_1.console.log("New Connection!!!");
             // - As compositor
-            // this.emit('connection', new Connection(this.currConnId++, this, socket, false));
+            this.emit('connection', new connection_js_1.Connection(this.currConnId++, this, socket, false));
             // - As MITM
-            const socket2 = new usocket_1.USocket({});
-            const conx = new connection_js_1.Connection(this.currConnId++, this, socket, true);
-            socket2.connect({ path: "/run/user/1000/wayland-0" }, () => {
-                socket.on("readable", () => {
-                    (0, connection_js_1.parseOnReadable)(socket, ({ data, fds }) => {
-                        // console.log("C2S", data && data.toString("hex"), fds);
-                        try {
-                            const [[a, b, c]] = [...conx.parser(data)];
-                            if (!blacklist.includes(a.iface)) {
-                                logger_js_1.console.log("C2S", connection_js_1.Connection.prettyWlObj(a), b, JSON.stringify(connection_js_1.Connection.prettyArgs(c)));
-                            }
-                            socket2.write({ data, fds });
-                        }
-                        catch (e) {
-                            logger_js_1.console.log('C2S got error', e);
-                            socket2.write({ data, fds });
-                        }
-                    });
-                });
-                socket2.on("readable", () => {
-                    (0, connection_js_1.parseOnReadable)(socket2, ({ data, fds }) => {
-                        // console.log("S2C", data && data.toString("hex"), fds);
-                        try {
-                            const [[a, b, c]] = [...conx.parser(data, true)];
-                            if (!blacklist.includes(a.iface)) {
-                                logger_js_1.console.log("S2C", connection_js_1.Connection.prettyWlObj(a), b, JSON.stringify(connection_js_1.Connection.prettyArgs(c)));
-                            }
-                            if (b === 'global' && !wl_registry_js_1.WlRegistry.supportedByRegistry.includes(c.interface))
-                                return logger_js_1.console.log('Ignoring');
-                            // if (!WlRegistry.registry.includes(c.))
-                            socket.write({ data, fds });
-                        }
-                        catch (e) {
-                            logger_js_1.console.log('S2C got error', e);
-                            socket.write({ data, fds });
-                        }
-                    });
-                });
-            });
+            // const socket2 = new USocket({});
+            // const conx = new Connection(this.currConnId++, this, socket, true);
+            // socket2.connect({ path: "/run/user/1000/wayland-0" }, () => {
+            //   socket.on("readable", () => {
+            //     parseOnReadable(socket, ({ data, fds }) => {
+            //       // console.log("C2S", data && data.toString("hex"), fds);
+            //       try {
+            //         const [[a, b, c]] = [...conx.parser(data)];
+            //         if (!blacklist.includes(a.iface)) {
+            //           console.log("C2S", Connection.prettyWlObj(a), b, JSON.stringify(Connection.prettyArgs(c)));
+            //         }
+            //         socket2.write({ data, fds });
+            //       } catch (e) {
+            //         console.log('C2S got error', e);
+            //         socket2.write({ data, fds });
+            //       }
+            //     });
+            //   });
+            //   socket2.on("readable", () => {
+            //     parseOnReadable(socket2, ({ data, fds }) => {
+            //       // console.log("S2C", data && data.toString("hex"), fds);
+            //       try {
+            //         const [[a, b, c]] = [...conx.parser(data, true)];
+            //         if (!blacklist.includes(a.iface)) {
+            //           console.log("S2C", Connection.prettyWlObj(a), b, JSON.stringify(Connection.prettyArgs(c)));
+            //         }
+            //         if (b === 'global' && !WlRegistry.supportedByRegistry.includes(c.interface)) return console.log('Ignoring');
+            //         // if (!WlRegistry.registry.includes(c.))
+            //         socket.write({ data, fds });
+            //       } catch (e) {
+            //         console.log('S2C got error', e);
+            //         socket.write({ data, fds });
+            //       }
+            //     });
+            //   });
+            // });
         }.bind(this));
         for (const event of ["exit", "SIGINT", "SIGTERM"]) {
             process.on(event, function () {
