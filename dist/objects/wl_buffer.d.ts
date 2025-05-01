0976dde6faba76a3948ccb046286e6c9da3a4677
Juliet WANG <informathemusic@gmail.com>
2025-05-02 00:37:55 +0200

    fix(shm->buffer): Build prev fix on shm

--- dist/objects/wl_buffer.d.ts
+++ dist/objects/wl_buffer.d.ts
@@ -11,5 +11,5 @@ export declare class WlBuffer extends BaseObject {
     wlRelease(): void;
     get pixelSize(): number;
     get size(): number;
-    getByte(i: number): Buffer<ArrayBufferLike>;
+    getByte(i: number): number;
 }
